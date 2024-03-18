70a85581f1600231c8ee947a49ea9d75595d33b5
HeliaShamszadeh <h.shamszadeh81@gmail.com>
2024-03-18 18:01:00 +0330

    [feat]: Added show/hide icon, added hover style for feilds and bottom border.

--- src/components/LoginSignUp/LoginSignup.jsx
+++ src/components/LoginSignUp/LoginSignup.jsx
@@ -2,18 +2,28 @@ import react from 'react'
 import React, { useState } from 'react';
 import './LoginSignup.css'
 
-import user_icon from '../../assets/person.png'
+import {Icon} from 'react-icons-kit';
+import {eyeOff} from 'react-icons-kit/feather/eyeOff';
+import {eye} from 'react-icons-kit/feather/eye'
+
 import lock_icon from '../../assets/password.png'
 import email_icon from '../../assets/email.png'
 
+
 const LoginContainer = () => {
   const [loginLabelsColor, setLoginLabelsColor] = useState(false);
+  const [_, setPassword] = useState("");
+  const [type, setType] = useState('password');
+  const [icon, setIcon] = useState(eyeOff);
 
   const handleSignupClick=() =>{
     const loginForm=document.querySelector("form.login");
     const loginText=document.querySelector(".header .login");
     loginForm.style.marginLeft="-50%";
     loginText.style.marginLeft="-50%";
+    setIcon(eyeOff);
+    setType('password')
+    setPassword("");
   };
 
   const handleLoginClick=() =>{
@@ -22,16 +32,28 @@ const LoginContainer = () => {
     loginForm.style.marginLeft="0%";
     loginText.style.marginLeft="0%";
     setLoginLabelsColor(!loginLabelsColor); 
+    setIcon(eyeOff);
+    setType('password')
+    setPassword("");
+
   };
 
-  const handleSignupLinkClick=(e) => {
-    e.preventDefault();
-    const signupBtn=document.querySelector("label.signup");
+  // const handleSignupLinkClick=(e) => {
+  //   e.preventDefault();
+  //   const signupBtn=document.querySelector("label.signup");
     
-    signupBtn.click();
+  //   signupBtn.click();
+  // };
+
+  const handleToggle = () => {
+    if (type === 'password') {
+      setIcon(eye);
+      setType('text');
+    } else {
+      setIcon(eyeOff);
+      setType('password');
+    }
   };
-
-
 
   return(
       <>
@@ -63,12 +85,18 @@ const LoginContainer = () => {
                             backgroundPosition: 'right'}}/>
                 </div>
                 <div className="field">
-                <input type="password" placeholder='رمز عبور' 
+                <input id="pass"
+                    type={type}
+                    name="password"
+                    placeholder='رمز عبور'
+                    autoComplete="current-password" 
                     style={{backgroundImage: `url(${lock_icon})`, 
                             backgroundRepeat: 'no-repeat', 
                             paddingRight: '40px',
-                            backgroundPosition: 'right'}}/>
+                            backgroundPosition: 'right'}}
+                            />
                 </div>
+                
                 <div className='pass_link'><a href="#" > فراموشی رمز عبور</a></div>
                 <div className="field btn">
                     <div className='btn_layer'></div>
@@ -83,7 +111,7 @@ const LoginContainer = () => {
                     <input type="text" placeholder='Name'/>
                   </div>*/}
                   <div className="field">
-                  <input type="text" placeholder='ایمیل' 
+                  <input type="email" placeholder='ایمیل' 
                     style={{backgroundImage: `url(${email_icon})`, 
                             backgroundRepeat: 'no-repeat', 
                             paddingRight: '40px',
@@ -91,12 +119,20 @@ const LoginContainer = () => {
                   </div>
                  
                   <div className="field">
-                  <input type="password" placeholder='رمز عبور' 
+                  <input id="pass"
+                    type={type}
+                    name="password"
+                    placeholder='رمز عبور'
+                    autoComplete="current-password" 
                     style={{backgroundImage: `url(${lock_icon})`, 
                             backgroundRepeat: 'no-repeat', 
                             paddingRight: '40px',
-                            backgroundPosition: 'right'}}/>
-                  </div>
+                            backgroundPosition: 'right'}}
+                            />
+                <span className="toggle-icon" onClick={handleToggle}>
+                <Icon icon={icon} size={23} />
+              </span>
+              </div>
                   <div className="field">
                   <input type="password" placeholder='تکرار رمز عبور' 
                     style={{backgroundImage: `url(${lock_icon})`, 
