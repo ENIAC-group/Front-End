c4b999d654e704be1285026d66e3dc2f0fd2aaab
Zahra Abbasgholi <z.abbasgholi10@gmail.com>
2024-04-04 16:54:33 +0330

    feat/Sidebar

--- src/components/SidebarNabar/NavBar_SideBar.jsx
+++ src/components/SidebarNabar/NavBar_SideBar.jsx
@@ -6,12 +6,20 @@ import { GrContactInfo } from "react-icons/gr";
 import { FaBars, FaBell, FaUserCircle } from "react-icons/fa";
 import styles from "./NavBar.module.css";
 
-
+import {
+  FaCog,
+  FaHome,
+  FaServicestack,
+  FaRegStickyNote,
+  FaRegFileAlt,
+} from "react-icons/fa";
+import styles1 from "./Sidebar.module.css";
 const NavBar_SideBar = () => {
   const navigate = useNavigate();
   const [sideBarToggle, setsideBarToggle] = useState(false);
   const handsidebarToggle = () => {
     setsideBarToggle(!sideBarToggle);
+    console.log("gfhj");
   };
   return (
     <>
@@ -52,7 +60,76 @@ const NavBar_SideBar = () => {
           <div style={{ width: "90px" }}></div>
           <FaBars className={styles.fBar} onClick={handsidebarToggle} />
         </div>
-      </div>      
+      </div>
+      {/* ----------------------------------------------------------------- */}
+      <div style={sideBarToggle ? { display: "block" } : { display: "none" }}>
+        <div className={styles1.side_body}>
+          <div className={styles1.side_p1}>
+            <h1 className={styles1.side_title}>داشبورد</h1>
+          </div>
+          <hr style={{ borderBlockColor: "white" }}></hr>
+          <ul className={styles1.side_list} >
+            <li
+              className={styles1.side_list_element}
+              onClick={(e)=>{
+                handsidebarToggle();
+                navigate("/Hme");
+              }}
+              
+            >
+              <label href="" className={styles1.side_list_element_text}>
+                <FaHome className={styles1.side_icons} /> خانه
+              </label>
+            </li>
+            <li className={styles1.side_list_element} 
+                onClick={(e)=>{
+                handsidebarToggle();
+                navigate("/Hme");
+              }}>
+              <label
+                href=""
+                className={styles1.side_list_element_text}
+              >
+                <FaRegFileAlt className={styles1.side_icons} /> تست ها
+              </label>
+            </li>
+            <li className={styles1.side_list_element} onClick={(e)=>{
+                handsidebarToggle();
+                navigate("/Test");
+              }}>
+              <label
+                href=""
+                className={styles1.side_list_element_text}
+              >
+                <FaServicestack className={styles1.side_icons} /> خدمات
+              </label>
+            </li>
+            <li className={styles1.side_list_element} onClick={(e)=>{
+                handsidebarToggle();
+                navigate("/--");
+              }}>
+              <label
+                href=""
+                className={styles1.side_list_element_text}
+              >
+                <FaRegStickyNote className={styles1.side_icons} /> معرفی
+              </label>
+            </li>
+            <li className={styles1.side_list_element} 
+                onClick={(e)=>{
+                handsidebarToggle();
+                navigate("/Setting");
+              }}>
+              <label
+                href=""
+                className={styles1.side_list_element_text}
+              >
+                <FaCog className={styles1.side_icons} /> تنظیمات
+              </label>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 };
