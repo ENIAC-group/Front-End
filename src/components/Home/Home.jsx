f9410d55455a55a9613da8a6df2144bd67716e03
HeliaShamszadeh <h.shamszadeh81@gmail.com>
2024-04-19 17:24:12 +0330

    [fix]: homepage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,12 +3,12 @@ import Slider from "../Slider/Slider.jsx";
 import Footer from "../Footer/Footer";
 import Statistic from "../Counter/Statistic.jsx";
 import Tests from "../Tests/Tests.jsx";
-import Doctors_Home from '../Doctor/doctor.jsx';
+import Doctors_Home from '../Doctor/doctor.jsx'
 
 function Home() {
   return (
     <>
-      <Slider/>
+      <Slider />
       <Doctors_Home/>
       <Statistic/>
       <Tests/>
