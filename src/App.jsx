1d0799df3a33fcf0b23f04f2f1f1194985aa4b46
HeliaShamszadeh <h.shamszadeh81@gmail.com>
2024-05-29 15:25:05 +0330

    feat: show ratings to doctor. also changed medical info modal

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ import ReservationPage from "./components/Reservation/Reservation.jsx";
 import DoctorPage from "./components/SeeingDoctorReservation/DoctorPage.jsx";
 import Patient_Panel from "./components/Patient_Panel/Patient_Panel.jsx";
 import PatientsList from "./components/Doctor's_Patients/PatientsList.jsx";
+import DoctorRating from "./components/DoctorPannelRating/DoctorPannelRating.jsx";
 
 
 function App() {
@@ -71,6 +72,10 @@ function App() {
         {" "}
         </Router.Route>
         <Router.Route path="/PatientsList" element={<PatientsList />}>
+
+          {" "}
+        </Router.Route>
+        <Router.Route path="/DoctorRatings" element={<DoctorRating />}>
 
           {" "}
         </Router.Route>
