8b8aa225e8e3408a31f7831562d2f113c1d86967
dehghan82 <108016220+dehghan82@users.noreply.github.com>
2024-07-04 00:11:39 +0330

    fix: change font size and margin

--- src/components/Recommendation/Recommendation.jsx
+++ src/components/Recommendation/Recommendation.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import "./Recommendation.css"; // Import your CSS file if you have styles
+import "./Recommendation.css";
 import first_image from "./img/psychology3.jpg";
 import second_image from "./img/psychology2.jpg";
 import { FcBusinessContact } from "react-icons/fc";
 
 const AboutSection = () => {
   return (
-    <div className="container-fluid py-5 my-5">
-      <div className="container pt-5">
-        <div className="row g-5" >
-          
+    <div className="container-fluid custom-container py-5 my-5">
+      <div className=" custom-container pt-5">
+        <div className="row g-5">
           <div
             className="col-lg-5 col-md-6 col-sm-12 wow fadeIn"
             data-wow-delay=".3s"
@@ -17,19 +16,18 @@ const AboutSection = () => {
               visibility: "visible",
               animationDelay: "0.3s",
               animationName: "fadeIn",
-             
             }}
           >
-            <div className="h-100 position-relative" >
+            <div className="h-100 position-relative">
               <img
                 src={first_image}
                 className="img-fluid w-75 rounded"
                 alt=""
-                style={{ marginBottom: "35%" }}
+                style={{ marginBottom: "30%" }}
               />
               <div
                 className="position-absolute w-75"
-                style={{ top: "35%", left: "35%" }}
+                style={{ top: "35%", left: "30%" }}
               >
                 <img
                   src={second_image}
@@ -49,27 +47,30 @@ const AboutSection = () => {
             }}
           >
             <h4 className="text-primary doctor_h2">راهنمای رزرو نوبت</h4>
-            <h1 className="mb-4 doctor_h2">پیشنهاد روان درمانگر</h1>
+            <h1 className="mb-4 doctor_h3">پیشنهاد روان درمانگر</h1>
             <p className="recom_text">
-              گروه روانشناسی اینیاک با بهره گیری از جدید ترین الگوریتم های هوش
-              مصنوعی موفق به طراحی سامانه پیشنهاد روان درمانگر گردیده است.
+              با قابلیت جدید اینیاک به کمک هوش مصنوعی دکتر مناسب خود را انتخاب
+              کنید
             </p>
-            <h5 className="text-primary doctor_h2">  پیشنهاد بهترین درمانگر برای شما بر اساس</h5>
+            <h5 className="text-primary doctor_h2">
+              {" "}
+              پیشنهاد بهترین درمانگر برای شما بر اساس
+            </h5>
             <p className=" recom_text">
-            <FcBusinessContact />
-            ملاحظات شخصی
+              <FcBusinessContact />
+              ملاحظات شخصی
             </p>
             <p className=" recom_text">
-            <FcBusinessContact />
-            سابقه درمانی 
+              <FcBusinessContact />
+              سابقه درمانی
             </p>
             <p className=" recom_text">
-            <FcBusinessContact />
-            تناسب تخصص پزشک و شرح حال شما 
+              <FcBusinessContact />
+              تناسب تخصص پزشک و شرح حال شما
             </p>
             <a
               href="#"
-              className="btn btn-secondary rounded-pill px-5 py-3 text-white"
+              className="btn btn-secondary rounded-pill  px-5 py-3 text-white"
             >
               شروع
             </a>
